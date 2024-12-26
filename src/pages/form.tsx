a2b234039fe3e6b76d4c4e6e91447f978c53cde9
RyannKim327 <rksesgundo123@gmail.com>
2024-12-26 22:27:18 +0800

    12-26-24 22:27

--- src/pages/form.tsx
+++ src/pages/form.tsx
@@ -1,10 +1,16 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { prop } from "../interfaces";
+import { faX } from "@fortawesome/free-solid-svg-icons";
 
 export default function Form(props: prop) {
   return (
-    <div className="flex flex-col justify-center items-center z-10 bg-[rgba(33,33,33,0.7)] fixed w-full h-full">
+    <div id="form-1" className="flex flex-col justify-center items-center z-10 bg-[rgba(33,33,33,0.7)] fixed w-full h-full">
       <form className="bg-white w-1/2 p-2 rounded-md">
-        <h1>Hello</h1>
+        <div className="flex justify-end p-2">
+          {/* header */}
+          <FontAwesomeIcon className="text-rose-700 hover:text-rose-900 cursor-pointer" icon={ faX } />
+        </div>
+        
       </form>
     </div>
   );
